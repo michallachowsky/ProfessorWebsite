813d4e6426626ccb47087318e9d638ed0b4699fb
michallachowsky <michal.lachowsky@gmail.com>
2021-12-13 11:49:52 +0200

    poprawkii

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -20,71 +20,68 @@ function AboutMe() {
             <video src='/videos/back.mp4' autoPlay loop muted />
             <h1>About Me</h1>
             {/* <img src="monitor.jpg" alt="" className='ekran'/> */}
-            <div className='desktop'>
-                <div className='edge'>
-                    <div className='it'>
-                        <div className='moreReact'>
-                            <img src="React.jpg" alt="" className='react' id="hobby"
-                            onClick={() => {
-                                setModalOpen(true);
-                            }}
-                            />
-                            {modalOpen && <Modal setOpenModal={setModalOpen} />}
-                            <h4>Learn more</h4>
-                        </div>
-                        <div className='morePython'>
-                            <img src="python.png" alt="" className='python' id="hobby"
-                            onClick={() => {
-                                setModalOpen1(true);
-                            }}
-                            />
-                            {modalOpen1 && <Modal1 setOpenModal1={setModalOpen1} />}
-                            <h4>Learn more</h4>
-                        </div>
-
-                        <div id="moreScrapp">
-                            <img src="web.png" alt="" className='web' id="hobby"
-                            onClick={() => {
-                                setModalOpen2(true);
-                            }}
-                            />
-                            {modalOpen2 && <Modal2 setOpenModal2={setModalOpen2} />}
-                            <h4>Learn more</h4>
-                        </div>
+            
+                <div className='it'>
+                    <div className='moreReact'>
+                        <img src="React.jpg" alt="" className='react' id="hobby"
+                        onClick={() => {
+                            setModalOpen(true);
+                        }}
+                        />
+                        {modalOpen && <Modal setOpenModal={setModalOpen} />}
+                        <h4>Learn more</h4>
+                    </div>
+                    <div className='morePython'>
+                        <img src="python.png" alt="" className='python' id="hobby"
+                        onClick={() => {
+                            setModalOpen1(true);
+                        }}
+                        />
+                        {modalOpen1 && <Modal1 setOpenModal1={setModalOpen1} />}
+                        <h4>Learn more</h4>
                     </div>
 
-                    <div className="other">
-                    <div className='moreFootball'>
-                            <img src="football.jpg" alt="" className='football' id="hobby"
-                            onClick={() => {
-                                setModalOpen3(true);
-                            }}
-                            />
-                            {modalOpen3 && <Modal3 setOpenModal3={setModalOpen3} />}
-                            <h4>Learn more</h4>
-                        </div>
-                        <div className='moreTravel'>
-                            <img src="travel.jpg" alt="" className='travel' id="hobby"
-                            onClick={() => {
-                                setModalOpen4(true);
-                            }}
-                            />
-                            {modalOpen4 && <Modal4 setOpenModal4={setModalOpen4} />}
-                            <h4>Learn more</h4>
-                        </div>
-
-                        <div id="moreCooking">
-                            <img src="cooking.jpg" alt="" id='cooking' id="hobby"
-                            onClick={() => {
-                                setModalOpen5(true);
-                            }}
-                            />
-                            {modalOpen5 && <Modal5 setOpenModal5={setModalOpen5} />}
-                            <h4>Learn more</h4>
-                        </div>
+                    <div id="moreScrapp">
+                        <img src="web.png" alt="" className='web' id="hobby"
+                        onClick={() => {
+                            setModalOpen2(true);
+                        }}
+                        />
+                        {modalOpen2 && <Modal2 setOpenModal2={setModalOpen2} />}
+                        <h4>Learn more</h4>
                     </div>
                 </div>
-            </div>
+
+                <div className="other">
+                <div className='moreFootball'>
+                        <img src="football.jpg" alt="" className='football' id="hobby"
+                        onClick={() => {
+                            setModalOpen3(true);
+                        }}
+                        />
+                        {modalOpen3 && <Modal3 setOpenModal3={setModalOpen3} />}
+                        <h4>Learn more</h4>
+                    </div>
+                    <div className='moreTravel'>
+                        <img src="travel.jpg" alt="" className='travel' id="hobby"
+                        onClick={() => {
+                            setModalOpen4(true);
+                        }}
+                        />
+                        {modalOpen4 && <Modal4 setOpenModal4={setModalOpen4} />}
+                        <h4>Learn more</h4>
+                    </div>
+
+                    <div id="moreCooking">
+                        <img src="cooking.jpg" alt="" id='cooking' id="hobby"
+                        onClick={() => {
+                            setModalOpen5(true);
+                        }}
+                        />
+                        {modalOpen5 && <Modal5 setOpenModal5={setModalOpen5} />}
+                        <h4>Learn more</h4>
+                    </div>
+                </div>
 
         </div>
     )
