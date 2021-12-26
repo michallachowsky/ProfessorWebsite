907888e5c1979b1bd9a12f2ae241942b54dc29af
michallachowsky <michal.lachowsky@gmail.com>
2021-12-26 14:56:24 +0100

    course

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Modal.css";
 
 function Modal({ setOpenModal }) {
@@ -20,7 +21,9 @@ function Modal({ setOpenModal }) {
         <div className="body">
           <p>Here You will learn the basics of React! Course is all in English. For the evalution 
               You will have to present project of website, all the details will be given during lectures.</p>
+          
         </div>
+        <Link to="/Course" className="readmore">Read more</Link>
         <div className="footer">
           <button
             onClick={() => {
--- src/pages/ReactPage.js
+++ src/pages/ReactPage.js
@@ -6,7 +6,23 @@ function ReactPage() {
         <div className='hero-container'>
             <video src='/videos/back.mp4' autoPlay loop muted />
             <img src="notebook.jpg" alt="" className='notebook'/>
-            
+            <div className='reactCourse'>
+                <h1>React course:</h1>
+                <div className='courseDescription'>
+                In this course, A Practical Start with React, you will first learn why React is so great. 
+                After this module you'll be able to determine if React is for you and/or your team. 
+                It will also provide a basic overview of the mechanics of a React app like tree reconciliation and the use of JSX. 
+                There's also a brief comparison with alternative libraries and frameworks included. Course is all in English. 
+                For the evalution You will have to present project of website, all the details will be given during lectures.
+                What we will use during course:
+                
+                </div>
+                <div className='whatweuse'>
+                    <img src="reactjs.jpeg" alt="" id="reactjs"/>
+                    <img src="html.png" alt="" id="reactjs"/>
+                    <img src="vsc.jpg" alt="" id="reactjs"/>
+                </div>
+            </div>
         </div>
     )
 }
